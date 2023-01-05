7333bdec406d74107b7d04b01c57c185cc592574
Constantin <catalin.varvaroi@gmail.com>
2023-01-05 10:38:45 +0200

    add auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import './App.css'
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      Hello.
-    </div>
+    <View className="App">
+      <Card>
+        <Heading level={1}>Amplify Auth</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
